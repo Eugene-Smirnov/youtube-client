e85264f3228d94c415ffb67073110a137495930d
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-08-05 22:36:59 +0300

    feat: start working on header component and stylisation

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 import { SearchItemComponent } from './search/search-item/search-item.component';
+import { HeaderSearchBarComponent } from './header/header-search-bar/header-search-bar.component';
+import { HeaderProfileComponent } from './header/header-profile/header-profile.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    HeaderSearchBarComponent,
+    HeaderProfileComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ app/src/app/header/header-profile/header-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-profile',
+  templateUrl: './header-profile.component.html',
+  styleUrls: ['./header-profile.component.scss'],
+})
+export class HeaderProfileComponent implements OnInit {
+  ngOnInit(): void {}
+}
