5617defb94cbbfa88590d505ef2ae908ee453f55
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-08-15 19:44:16 +0300

    feat: implement 404 page without router logic

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/pages/login/login.component';
+import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 import { SearchResultsComponent } from './youtube/pages/search-results/search-results.component';
 
 const routes: Routes = [
+  { path: '404', component: NotFoundComponent },
   { path: 'login', component: LoginComponent },
   { path: 'search', component: SearchResultsComponent },
 ];
--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeaderSearchBarComponent } from './components/header/header-search-bar/header-search-bar.component';
 import { HeaderProfileComponent } from './components/header/header-profile/header-profile.component';
 import { HeaderSearchParamsComponent } from './components/header/header-search-params/header-search-params.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { HeaderSearchParamsComponent } from './components/header/header-search-p
     HeaderSearchBarComponent,
     HeaderProfileComponent,
     HeaderSearchParamsComponent,
+    NotFoundComponent,
   ],
   imports: [CommonModule],
   exports: [HeaderComponent],
